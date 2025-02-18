16fcf3f15882d2e69dfaec0f5168cd2f311fa74d
meagle21 <emanuel.eagle@gmail.com>
2025-02-18 10:28:00 -0500

    update

--- global-vars.tf
+++ global-vars.tf
@@ -1,9 +1,9 @@
-variable "container-repo-name" {
+variable "container_repo_name" {
   type = string
   default = "home-automation-container-repository"
 } 
 
-variable "default-region" {
+variable "default_region" {
   type = string
   default = "us-east-2"
 } 
--- weather_alerts/weather_alerts.py
+++ weather_alerts/weather_alerts.py
@@ -1,1 +1,25 @@
 import requests 
+import os
+
+def rain_check(forecast_content):
+    precip_info = forecast_content["probabilityOfPrecipitation"]
+    if precip_info["value"] >= 30:
+        return True
+    return False
+
+def lambda_handler(event, context):
+    nws_url = os.environ["NWS_URL"]
+    alert_range = os.environ["ALERT_RANGE"]
+    ifttt_webhook = os.environ["WEBHOOK_URL"]
+    rain_alert_sent = False
+    weather_request = requests.get(nws_url)
+    weather_response_content = weather_request.json()["properties"]["periods"][:alert_range]
+    for hourly_forecast in weather_response_content:
+        forecast_time = hourly_forecast["startTime"]
+        if rain_check(hourly_forecast) and rain_alert_sent == False:
+            rain_alert_sent = True
+            requests.post(ifttt_webhook)
+    return {
+            "statusCode": weather_request.status_code,
+            "message": "Successfully ran alerting lambda."
+        }
