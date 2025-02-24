6960c35465bae3b2731954b9f869054a0e4bc10a
meagle21 <emanuel.eagle@gmail.com>
2025-02-23 22:02:34 -0500

    update

--- weather_alerts/infra/iam.tf
+++ weather_alerts/infra/iam.tf
@@ -1,49 +1,49 @@
-# data "aws_iam_policy_document" "assume_role" {
-#   statement {
-#     effect = "Allow"
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["lambda.amazonaws.com"]
-#     }
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
 
-#     actions = ["sts:AssumeRole"]
-#   }
-# }
+    actions = ["sts:AssumeRole"]
+  }
+}
 
-# resource "aws_iam_role" "iam_for_lambda" {
-#   name               = "iam_for_lambda"
-#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-# }
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
 
-# data "aws_iam_policy_document" "lambda_policy_document" {
+data "aws_iam_policy_document" "lambda_policy_document" {
 
-#   statement {
-#     effect    = var.allow_string
-#     actions   = var.ecr_permissions_list
-#     resources = [aws_ecr_repository.weather-alerts.arn]
-#   }
+  statement {
+    effect    = var.allow_string
+    actions   = var.ecr_permissions_list
+    resources = [aws_ecr_repository.weather-alerts.arn]
+  }
 
-# }
+}
 
-# resource "aws_iam_policy" "lambda_policy" {
-#   name   = "lambda-policy"
-#   policy = data.aws_iam_policy_document.lambda_policy_document.json
-# }
+resource "aws_iam_policy" "lambda_policy" {
+  name   = "lambda-policy"
+  policy = data.aws_iam_policy_document.lambda_policy_document.json
+}
 
-# resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-#   role       = aws_iam_role.iam_for_lambda.name
-#   policy_arn = aws_iam_policy.lambda_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
 
 
 
-# variable ecr_permissions_list {
-#     description = "Permissions for the Lambda function"
-#     type = list
-#     default = ["ecr:BatchGetImage", "ecr:GetDownloadUrlForLayer"]
-# }
+variable ecr_permissions_list {
+    description = "Permissions for the Lambda function"
+    type = list
+    default = ["ecr:BatchGetImage", "ecr:GetDownloadUrlForLayer"]
+}
 
-# variable allow_string {
-#   default = "Allow"
-# }
+variable allow_string {
+  default = "Allow"
+}
--- weather_alerts/infra/weather-alerts-vars.tf
+++ weather_alerts/infra/weather-alerts-vars.tf
@@ -1,19 +1,19 @@
-# variable RAIN_NOTIFICATION_WEBHOOK {
-#     sensitive = true
-#     type = string
-# }
+variable RAIN_NOTIFICATION_WEBHOOK {
+    sensitive = true
+    type = string
+}
 
-# variable ALERT_RANGE {
-#     type = number
-#     default = 18
-# }
+variable ALERT_RANGE {
+    type = number
+    default = 18
+}
 
-# variable NWS_URL_BOS {
-#     type = string
-#     default  = "https://api.weather.gov/gridpoints/BOX/42,71/forecast/hourly?units=us" 
-# }
+variable NWS_URL_BOS {
+    type = string
+    default  = "https://api.weather.gov/gridpoints/BOX/42,71/forecast/hourly?units=us" 
+}
 
-# variable lambda_function_name {
-#     type = string
-#     default = "weather_alerts"
-# }
+variable lambda_function_name {
+    type = string
+    default = "weather_alerts"
+}
