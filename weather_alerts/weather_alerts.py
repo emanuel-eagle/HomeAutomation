2ab1570abda6d4c2662015bcb9f62c1127890ba6
meagle21 <emanuel.eagle@gmail.com>
2025-02-25 12:38:59 -0500

    update

--- weather_alerts/weather_alerts.py
+++ weather_alerts/weather_alerts.py
@@ -15,12 +15,12 @@ def lambda_handler(event, context):
     print("got environmental variables")
     rain_alert_sent = False
     weather_request = requests.get(nws_url)
-    weather_response_content = weather_request.json()["properties"]["periods"][:alert_range]
-    for hourly_forecast in weather_response_content:
-        forecast_time = hourly_forecast["startTime"]
-        if rain_check(hourly_forecast) and rain_alert_sent == False:
-            rain_alert_sent = True
-            requests.post(ifttt_webhook)
+    # weather_response_content = weather_request.json()["properties"]["periods"][:alert_range]
+    # for hourly_forecast in weather_response_content:
+    #     forecast_time = hourly_forecast["startTime"]
+    #     if rain_check(hourly_forecast) and rain_alert_sent == False:
+    #         rain_alert_sent = True
+    requests.post(ifttt_webhook)
     return {
             "statusCode": weather_request.status_code,
             "message": "Successfully ran alerting lambda."
